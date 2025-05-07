5c1c43e430eef3e71402b3171d760b7f2d50eeb6
sangdzne <huynhthanhsang233@gmail.com>
2025-05-07 20:34:54 +0700

    Update appstate.js

--- appstate.js
+++ appstate.js
@@ -1,1 +1,135 @@
-//thích thì add appstate vô để cho nó chạy command
+	{
+		"key": "dbln",
+		"value": "%7B%2261562096853038%22%3A%22kKQ8hAQh%22%2C%2261573433281163%22%3A%22fvnnQSpb%22%2C%2261573568190854%22%3A%22Ql194Z7r%22%2C%2261573453872673%22%3A%220oseiu5t%22%2C%2261573798108685%22%3A%22piONkmyP%22%2C%2261572789819410%22%3A%22guMkEaXY%22%2C%2261573868218464%22%3A%22XeqtkC3y%22%2C%2261573552750758%22%3A%22vHSL510W%22%2C%2261574614180915%22%3A%22NoPunkl6%22%2C%2261574947464475%22%3A%22P2Ic2jpi%22%2C%2261574219595437%22%3A%22pbf5LcO3%22%2C%2261574498086607%22%3A%2258jgyLq5%22%2C%2261554658260560%22%3A%226UOf1X0p%22%7D",
+		"domain": "facebook.com",
+		"path": "/login/device-based/",
+		"hostOnly": false,
+		"creation": "2025-05-07T13:33:30.545Z",
+		"lastAccessed": "2025-05-07T13:33:30.565Z"
+	},
+	{
+		"key": "datr",
+		"value": "u9q9Z7Imd7RQ9liJ-H_RZQmc",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-05-07T13:33:30.565Z",
+		"lastAccessed": "2025-05-07T13:33:30.565Z"
+	},
+	{
+		"key": "sb",
+		"value": "u9q9Z0Osu9IMIfxyfDUw_o_5",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-05-07T13:33:30.565Z",
+		"lastAccessed": "2025-05-07T13:33:30.565Z"
+	},
+	{
+		"key": "ps_l",
+		"value": "1",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-05-07T13:33:30.565Z",
+		"lastAccessed": "2025-05-07T13:33:30.565Z"
+	},
+	{
+		"key": "ps_n",
+		"value": "1",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-05-07T13:33:30.565Z",
+		"lastAccessed": "2025-05-07T13:33:30.565Z"
+	},
+	{
+		"key": "oo",
+		"value": "v1",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-05-07T13:33:30.565Z",
+		"lastAccessed": "2025-05-07T13:33:30.565Z"
+	},
+	{
+		"key": "locale",
+		"value": "vi_VN",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-05-07T13:33:30.565Z",
+		"lastAccessed": "2025-05-07T13:33:30.565Z"
+	},
+	{
+		"key": "c_user",
+		"value": "61574614180915",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-05-07T13:33:30.565Z",
+		"lastAccessed": "2025-05-07T13:33:30.565Z"
+	},
+	{
+		"key": "xs",
+		"value": "1%3AYn-TbHFiKBcnVQ%3A2%3A1746542789%3A-1%3A-1",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-05-07T13:33:30.565Z",
+		"lastAccessed": "2025-05-07T13:33:30.565Z"
+	},
+	{
+		"key": "fbl_st",
+		"value": "100627563%3BT%3A29109899",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-05-07T13:33:30.565Z",
+		"lastAccessed": "2025-05-07T13:33:30.565Z"
+	},
+	{
+		"key": "wl_cbv",
+		"value": "v2%3Bclient_version%3A2806%3Btimestamp%3A1746593949",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-05-07T13:33:30.565Z",
+		"lastAccessed": "2025-05-07T13:33:30.565Z"
+	},
+	{
+		"key": "vpd",
+		"value": "v1%3B622x553x1.306249976158142",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-05-07T13:33:30.565Z",
+		"lastAccessed": "2025-05-07T13:33:30.565Z"
+	},
+	{
+		"key": "dpr",
+		"value": "1.436152458190918",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-05-07T13:33:30.565Z",
+		"lastAccessed": "2025-05-07T13:33:30.565Z"
+	},
+	{
+		"key": "wd",
+		"value": "891x1715",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-05-07T13:33:30.565Z",
+		"lastAccessed": "2025-05-07T13:33:30.565Z"
+	},
+	{
+		"key": "fr",
+		"value": "0Bxh5VGyEf3niUy79.AWeZlPgpj1ao6I5zihv6Q_7llFLtJSJknNPfCih0G_BzKLH9Dhs.Bn6SUz..AAA.0.0.BoG2EP.AWdqjdducqIOt78Q6xWcSwQPGnc",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-05-07T13:33:30.565Z",
+		"lastAccessed": "2025-05-07T13:33:30.565Z"
+	}]
